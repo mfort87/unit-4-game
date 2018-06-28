9a0290ac6758172f4411d7df07e974a5ae6e2aff
Matt Fort <mfort8487@gmail.com>
2018-06-28 15:56:28 -0500

    can't get white gem to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,3 +14,67 @@ var yellowGem = 1 + Math.floor(Math.random() * 12);
 var wins = 0;
 var losses = 0;
 var total = 0;
+
+$("#losses").html(losses);
+$("#wins").html(wins);
+$("#totalNumber").html(total);
+
+
+//this should reselt variables when game is done
+
+var reset = function (){
+    total = 0;
+    randomNumber= 19 + Math.floor(Math.random() * 120);
+    rainbowGem = 1 + Math.floor(Math.random() * 12);
+    blueGem = 1 + Math.floor(Math.random() * 12);
+    whiteGem = 1 + Math.floor(Math.random() * 12);
+    yellowGem = 1 + Math.floor(Math.random() * 12);
+    $("#losses").html(losses);
+    $("#wins").html(wins);
+    $("#totalNumber").html(total);
+
+
+}
+
+
+var winning = function(){
+    if (total == randomNumber){
+        wins++;
+        alert("Woo! You're a winner!")
+        $("#wins").html(wins);
+        reset();
+    
+    } else if (total > randomNumber){
+        losses++;
+        alert("Aw Shucks, you didn't win. Try Again!");
+        $("#losses").html(losses);
+        reset();
+    }
+};
+
+//adding number to total score when gem is clicked on
+
+$("#rainbow").on("click",function(){
+    total = total + rainbowGem;
+    $("#totalNumber").html(total);
+    winning();
+})
+
+$("#blue").on("click", function(){
+    total = total + blueGem;
+    $("#totalNumber").html(total);
+    winning();
+
+})
+
+$("#yellow").on("click", function (){
+    total = total + yellowGem;
+    $("#totalNumber").html(total);
+    winning();
+})
+
+$("#white").on("clilck", function(){
+    total = total + whiteGem;
+    $("#totalNumber").html(total);
+    winning();
+})
